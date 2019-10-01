47d483f875a66e1e9581f97aea730be84ff7bd24
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-01 14:43:43 +0530

    day 2 exercise 1 finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(),
-      ),
-    ),
+  Container container_1 = Container(
+    width: 100,
+    height: 100,
+
+    color: Colors.red,
+
+
   );
-}
+
+
+  Container container_2 = Container(
+
+    width: 100,
+    color: Colors.blue,
+
+
+  );
+  Container container_3 = Container(
+
+    width: 100,
+    height: 100,
+    color: Colors.yellow,
+
+
+  );
+  Container container_4 = Container(
+
+    width: 100,
+    height: 100,
+    color: Colors.green,
+
+
+  );
+  List<Widget> contain2 = [container_3, container_4];
+  List<Widget> contain1 = [container_3, container_4];
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.purple,
+          body: SafeArea(
+            child: Row(
+
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[container_1,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: contain2,
+                ),
+                container_2
+              ],
+            ),
+          ),
+
+
+        ),
+
+      ),);
+  }
